4ba12aebbe9934d0a7e47458a5bf0e1ffc4c6de9
Wolfgang Huesgen <wolfgang.huesgen@web.de>
2023-04-16 10:15:46 +0100

    test3

--- src/index.js
+++ src/index.js
@@ -1,16 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-const el = document.getElementById('root');
+const el = document.getElementById("root");
 const root = ReactDOM.createRoot(el);
 
 function App() {
-    return (
-      <div>
-        <h1>Hi there!</h1>;
-        <p>The next day!</p>
-      </div>
-    ); 
+  return (
+    <div>
+      <h1>Hi there!</h1>
+      <p>The next day!</p>
+    </div>
+  );
 }
 
 root.render(<App />);
